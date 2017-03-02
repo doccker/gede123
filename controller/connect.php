78a29c0265b63e6f36465163f1675a5e5b4529c6
sunmnaobin <sunmnaobin@bbktel.com>
2017-03-02 15:29:06 +0800

    fix config bug

--- controller/connect.php
+++ controller/connect.php
@@ -3,7 +3,7 @@ $timezone = "Asia/Shanghai";
 
 error_reporting(E_ALL ^ E_DEPRECATED);
 
-$con=mysql_connect("localhost","root","night2007");
+$con=mysql_connect("xxx","xx","xxxx");
 mysql_select_db("gede123",$con);
 
 //$con=mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
