276c9cd4d420705656bf515791d4d8c9b5aaa121
sunmaobin <night2007>
2017-10-26 10:19:22 +0800

    优化系统

--- controller/connect.php
+++ controller/connect.php
@@ -3,7 +3,7 @@ $timezone = "Asia/Shanghai";
 
 error_reporting(E_ALL ^ E_DEPRECATED);
 
-$con=mysql_connect("120.76.251.109","root","gede@123");
+$con=mysql_connect("xxx","xx","xxx");
 mysql_select_db("gede123",$con);
 
 //$con=mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
